ed67a76684fcc1327c76e9d79e924c70ed78c76d
rgutha <rgutha@DIN66008038.corp.capgemini.com>
2017-09-15 16:17:54 +0530

    Second Commit

--- demo/src/main/java/com/example/demo/HomeController.java
+++ demo/src/main/java/com/example/demo/HomeController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class HomeController {
 	@RequestMapping("/hello")  
-    public String hello(){  
+    public String hello(){ 
+		System.out.println("Hello");
         return"Hello!";  
 }
 }
